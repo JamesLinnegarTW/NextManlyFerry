53d39cdc7f1fd6034889ea5f45115c5f40d2ab8e
James Linnegar <jlinnega@thoughtworks.com>
2014-07-04 16:21:06 +1000

    james testing update webdriver

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-protractor-runner');
-  grunt.loadNpmTasks('grunt-protractor-webdriver');
 
   grunt.initConfig({
     shell: {
@@ -68,7 +67,7 @@ module.exports = function(grunt) {
         your_target: {
             options: {
                 path: 'node_modules/protractor/bin/',
-                command: 'webdriver-manager start'
+                command: 'webdriver-manager start --standalone'
             }
         }
     },
